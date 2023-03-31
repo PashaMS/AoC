946d9d20da9dc4f3de28f707079665bc14ae3861
foroudip <foroudip@gene.com>
2023-03-30 18:54:47 -0700

    adding spaces for cleanliness :)

--- AoC_2022/day1.R
+++ AoC_2022/day1.R
@@ -4,7 +4,9 @@
 input <- readLines("~/AoC/AoC_2022/data/day1_input.txt", warn = F)
 ind <- which(input == "")
 # input_num <- as.numeric(input) |> na.omit()
+
 splitAt <- function(x, pos) unname(split(x, cumsum(seq_along(x) %in% pos)))
+
 lapply(splitAt(input, ind), function(x) sum(as.numeric(x), na.rm = T)) |> 
   unlist() |>
   max()
